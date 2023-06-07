48490e604f3d5f3dd3c809725dcd7e99a452c591
Frumentius <frumentius_sonic@yahoo.com>
2023-06-07 21:20:10 +0700

    Add Name to Welcome.

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UserObjType } from '../type/type-interfaces';
 
 @Component({
   selector: 'app-welcome',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./welcome.component.css']
 })
 export class WelcomeComponent {
-
+  userData: null | UserObjType;
+  constructor(){
+    this.userData = null;
+    let tempData = localStorage.getItem('userData');
+    if(tempData) this.userData = JSON.parse(tempData);
+  }
 }
