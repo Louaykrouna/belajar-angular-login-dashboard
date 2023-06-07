ae6480cd2c2119e07f7975fe1b385c9677ab283b
Frumentius <frumentius_sonic@yahoo.com>
2023-06-07 20:11:55 +0700

    Reactive Form Login, dan Validator nya.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { inputPasswordValidator } from '../validator/input-validator';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit, OnDestroy {
+  copyYear: number;
+  myForm!: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {
+    let tgl = new Date();
+    this.copyYear = tgl.getFullYear();
+  }
+
+  ngOnInit(): void {
+    this.myForm = this.formBuilder.group({
+      floatingEmail: [
+        '',
+        {
+          validators: [Validators.required, Validators.email],
+          updateOn: 'blur'
+        }
+      ],
+      floatingPassword: [
+        '',
+        {
+          validators: [Validators.required, inputPasswordValidator()],
+          updateOn: 'submit'
+        }
+      ]
+    });
+  }
+
+  get floatingEmail() {
+    return this.myForm.get('floatingEmail');
+  }
+  get floatingPassword() {
+    return this.myForm.get('floatingPassword');
+  }
+
+  onSubmit(): void {
+    if (this.myForm.valid) {
+      console.log(this.myForm.value);
+      // Perform further actions with the form data
+    }
+  }
+
+  ngOnDestroy(): void {
+    
+  }
+}
+interface UserObj {
+  id: number;
+  name: string;
+  email: string;
+  password: string;
 }
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 
+import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -11,7 +13,9 @@ import { LoginComponent } from './login.component';
   ],
   imports: [
     CommonModule,
-    LoginRoutingModule
+    ReactiveFormsModule,
+    LoginRoutingModule,
+    NgbAlertModule
   ]
 })
 export class LoginModule { }
